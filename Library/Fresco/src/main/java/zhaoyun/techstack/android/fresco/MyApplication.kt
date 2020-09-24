13632f376977df199253a7581aca523d9f7661ec
zhaoyun <zhaoyun1224@gmail.com>
2020-09-24 14:12:46 +0800

    Fresco playground

---
+++ Library/Fresco/src/main/java/zhaoyun/techstack/android/fresco/MyApplication.kt
@@ -0,0 +1,13 @@
+package zhaoyun.techstack.android.fresco
+
+import android.app.Application
+import com.facebook.drawee.backends.pipeline.Fresco
+
+class MyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Fresco.initialize(this)
+    }
+}
